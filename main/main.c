1df9e9c48dfcc233764a769ff0b9feace9c8e8cd
JuanDSanchez18 <juandsanchez1998@gmail.com>
2021-02-20 19:10:27 -0500

    Final

--- main/main.c
+++ main/main.c
@@ -1,3 +1,6 @@
+/*ESP32 captador de paquestes IEEE802.11, filtrado para obtener unicamente paquetes Probe Request con una cobertura
+de 10 metros, también está configurado como punto de accesso. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,24 +14,22 @@
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 
-#define CHANNEL 7
+#define CHANNEL 6
 #define CHANNEL_HOPPING true //if true it will scan on all channels
 #define MAX_CHANNEL 13 //(only necessary if channelHopping is true)
-#define HOP_INTERVAL 240//in ms (only necessary if channelHopping is true)
-
+#define HOP_INTERVAL 200//in ms (only necessary if channelHopping is true)
 
 int ch = CHANNEL;
 
-// Filters Probe Request y RSSI
-const char *probe_request = "40";
-int minrssi = -80;
+const char *probe_request = "40"; // Subtipo paquete en frame
+int minrssi = -60; // Filtro al RSSI, Cobertura de XX metros
 
 static esp_err_t event_handler(void *ctx, system_event_t *event);
 
 static void Wifi_Sniffer(void);
 static void sniffer(void *buff, wifi_promiscuous_pkt_type_t type);
 
-//packet sniffer
+// Recibe todas las capturas y aplica los filtros
 void sniffer(void *buf, wifi_promiscuous_pkt_type_t type){
   wifi_promiscuous_pkt_t* pkt = (wifi_promiscuous_pkt_t*)buf;
   wifi_pkt_rx_ctrl_t rx_ctrl = (wifi_pkt_rx_ctrl_t)pkt->rx_ctrl; //Metadata header
@@ -39,7 +40,8 @@ void sniffer(void *buf, wifi_promiscuous_pkt_type_t type){
 
   strupr(subtype);
 
-  //printf("mgmt subtype: %02X, aux: %s\n",pkt->payload[0],subtype);
+  //printf("mgmt subtype: %01x, aux: %s\n",pkt->payload[0],subtype);
+  //printf ("% \n",pkt->payload[0]);
 
   if ((strcmp(subtype,probe_request) == 0) && (rx_ctrl.rssi > minrssi)){
 
@@ -65,11 +67,13 @@ const wifi_promiscuous_filter_t filt = {//Set promiscuous filter
 					.filter_mask =  WIFI_PROMIS_FILTER_MASK_MGMT
 		};
 
+
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
 }
 
+// Configuración ESP32
 static void Wifi_Sniffer(void)
 {
 	nvs_flash_init();
@@ -82,10 +86,10 @@ static void Wifi_Sniffer(void)
 	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_AP) );
 	wifi_config_t ap_config = {
 				.ap = {
-					.ssid = "Esp32_Serial1",
+					.ssid = "Esp32_EStacion2",
 					.ssid_len = 0,
-					.password = "STUTM_Calle45",
-					.channel = 6,
+					.password = "STUTM_Estacion2",
+					.channel = 4,
 					.authmode = WIFI_AUTH_WPA2_PSK,
 					.ssid_hidden = 0,
 					.max_connection = 4,
